f4f10bc92bbda47ca174488bcf435025017e8606
Alexey <113031830+AlexBorQA@users.noreply.github.com>
2022-10-31 12:22:37 +0500

    изменил девиз

--- js.js
+++ js.js
@@ -3,5 +3,5 @@ const button = document.querySelector('button');
 
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
 button.addEventListener('click', function() {
-	alert('Довожу все до конца')
+	alert('Всегда довожу все до конца')
 })
