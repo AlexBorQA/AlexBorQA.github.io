cb118d7d735809bf9f7ef5f2a8e55ddde16b260c
Alexey <113031830+AlexBorQA@users.noreply.github.com>
2022-11-24 14:36:51 +0500

    On russion

--- js.js
+++ js.js
@@ -3,5 +3,5 @@ const button = document.querySelector('button');
 
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
 button.addEventListener('click', function() {
-	alert('I always finish everything')
+	alert('Всегда довожу все до конца.')
 })
